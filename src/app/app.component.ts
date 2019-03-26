051e2cdbf2cf03e9aba63e1a77ab240f0b042d60
kevinash92 <esselebomebara@gmail.com>
2019-03-26 13:53:00 +0100

    implémentation d'auth.service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import * as firebase from 'firebase' 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  constructor(){
+    var config = {
+    apiKey: "AIzaSyBsAevOgfqfnZ00T5wjLz9qUU3S2d2pavo",
+    authDomain: "bookshelves-b546f.firebaseapp.com",
+    databaseURL: "https://bookshelves-b546f.firebaseio.com",
+    projectId: "bookshelves-b546f",
+    storageBucket: "bookshelves-b546f.appspot.com",
+    messagingSenderId: "473930698903"
+  };
+  firebase.initializeApp(config);
+  }
 }
